6a0a5ff716961dcb2a0a20653455d8c09874afba
Rafael Camara <rafaelcamarac@gmail.com>
2022-10-10 23:29:20 +0100

    Handle post requests.

--- NatoursAPI/app.js
+++ NatoursAPI/app.js
@@ -1,6 +1,11 @@
 const fs = require('fs');
 const express = require('express');
+const { json } = require('express');
 const app = express();
+
+//allows us to have access to the request's body
+//if not present, req.body is undefined
+app.use(express.json());
 
 const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/data/tours-simple.json`)
@@ -11,6 +16,18 @@ app.get("/api/v1/tours", (req,res) => {
         status: 'success',
         results: tours.length,
         data: tours
+    });
+});
+
+app.post("/api/v1/tours", (req,res) => {
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({id:newId}, req.body);
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).send({
+            status: 'success',
+            data: newTour
+        });
     });
 });
 
