71163c2eda20e2b83d83f2a7dc95a885bf679251
Rafael Camara <rafaelcamarac@gmail.com>
2022-10-16 22:22:31 +0100

    Add middleware notes and fix route.

--- NatoursAPI/app.js
+++ NatoursAPI/app.js
@@ -4,9 +4,26 @@ const { json } = require('express');
 const { create } = require('domain');
 const app = express();
 
-//allows us to have access to the request's body
-//if not present, req.body is undefined
+// allows us to have access to the request's body
+// if not present, req.body is undefined
+// the .use method allows us to use middleware (add middleware to our middleware stack)
 app.use(express.json());
+
+/**
+ * We can define our custom middlewares.
+ */
+app.use((req, res, next) => {
+    /**
+     * We can add custom properties to the request object and use those in our routers definition (or in another middleware upfront)
+     */
+    req.requestTime = new Date().toISOString();
+
+    /**
+     * We must always call the next method
+     * If we don't call it, this will stuck on the middleware pipeline, thus not returning any response to the user
+     */
+    next();
+});
 
 const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/data/tours-simple.json`)
@@ -51,7 +68,7 @@ const createTour = (req,res) => {
 app
     .route("/api/v1/tours")
         .get(getTours)
-        .post(create);
+        .post(createTour);
 
 app
     .route("/api/v1/tours/:id")
