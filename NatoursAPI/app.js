e4268fa4043151a09581d0b04730920641d621e7
Rafael Camara <rafaelcamarac@gmail.com>
2022-10-16 21:40:48 +0100

    Refactor routes.

--- NatoursAPI/app.js
+++ NatoursAPI/app.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const express = require('express');
 const { json } = require('express');
+const { create } = require('domain');
 const app = express();
 
 //allows us to have access to the request's body
@@ -11,15 +12,31 @@ const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/data/tours-simple.json`)
 );
 
-app.get("/api/v1/tours", (req,res) => {
+const getTours = (req,res) => {
     res.status(200).send({
         status: 'success',
         results: tours.length,
         data: tours
     });
-});
+}
 
-app.post("/api/v1/tours", (req,res) => {
+const getTourById = (req,res) => {
+    const id = req.params.id * 1; //multiplying by one makes the result type of number
+    const tour = tours.find(el => el.id === id);
+    if(!tour) {
+        return res.status(404).json({
+            status: 'failed',
+            message: 'Invalid ID'
+        });
+    }
+
+    res.status(200).json({
+        status: 'success',
+        data: tour
+    });
+}
+
+const createTour = (req,res) => {
     const newId = tours[tours.length - 1].id + 1;
     const newTour = Object.assign({id:newId}, req.body);
     tours.push(newTour);
@@ -29,7 +46,16 @@ app.post("/api/v1/tours", (req,res) => {
             data: newTour
         });
     });
-});
+}
+
+app
+    .route("/api/v1/tours")
+        .get(getTours)
+        .post(create);
+
+app
+    .route("/api/v1/tours/:id")
+        .get(getTourById);
 
 const port = 3000;
 app.listen(port, ()=>{
